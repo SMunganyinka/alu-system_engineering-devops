b29fd3241623dcf2c0172e302195580d25d8c39f
SMunganyinka <s.munganyin@alustudent.com>
2025-07-07 14:20:50 +0000

    update

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-# Fix Apache 500 error
+# 'Fixing wordpress issue that have  the .phpp extension to.php'
+
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin:/usr/bin:/bin',
+  path    => '/usr/local/bin/:/bin/'
 } 
